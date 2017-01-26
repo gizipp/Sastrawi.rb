37b7650f5292c36c07ce410b89bf9a40e0f496f3
Gilang R. Aprianto <gizipp.gizipp@gmail.com>
2017-01-26 11:56:21 +0700

    Initialize Dictionary

--- lib/sastrawi/dictionary.rb
+++ lib/sastrawi/dictionary.rb
@@ -1,4 +1,26 @@
 module Sastrawi
-  class Dictonary
+  class Dictionary
+    BASE_WORDS = File.read(Dir.pwd + '/lib/sastrawi/data/kata-dasar.txt').split("\n")
+    attr_accessor :words
+
+    def initialize
+      @words = []
+    end
+
+    def contains word
+      @words.include? word
+    end
+
+    def count
+      @words.count
+    end
+
+    def add_words words
+      words.each{ |word| add word }
+    end
+
+    def add word
+      @words << word
+    end
   end
 end
